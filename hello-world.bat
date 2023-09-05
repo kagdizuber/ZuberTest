9de8598f258bb3c4402c837f2be2fffd9370f517
yellaiah a <yellaiahdevops@gmail.com>
2023-09-05 08:49:16 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -2,4 +2,4 @@
 echo Hello world!
 echo yellaiah test!
 echo "harmanpgs"
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://gitlab.com/new370/test1/-/blob/main/OpenSC-0.18.0-win32_vs12-Release.msi -OutFile C:\xxx.msi;}"
+powershell -Command "Get-Date"
