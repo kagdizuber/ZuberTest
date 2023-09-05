9cf5e53d1d35735105ddfae2e45108bf4e6dc151
yellaiah a <yellaiahdevops@gmail.com>
2023-09-05 14:49:42 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -1,4 +1,10 @@
-@echo off 
-cd
-pwd
-echo "test"
+@echo off
+echo Hello World from %AGENT_NAME%.
+echo My ID is %AGENT_ID%.
+echo AGENT_WORKFOLDER contents:
+@dir %AGENT_WORKFOLDER%
+echo AGENT_BUILDDIRECTORY contents:
+@dir %AGENT_BUILDDIRECTORY%
+echo BUILD_SOURCESDIRECTORY contents:
+@dir %BUILD_SOURCESDIRECTORY%
+echo Over and out.
