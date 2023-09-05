35c84d9d8c804a22e16a5e4b41b3d6cf1bbbf3cd
yellaiah a <yellaiahdevops@gmail.com>
2023-09-05 07:21:01 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -1,12 +1,2 @@
-@(
-    Echo On
-    Title CMD Window with SVN enabled
-    Set "PATH=%PATH%;D:\ummy\directory"
-)
-svn --version --quiet
-@(
-    Rem All other none echoed commands here
-    Pause
-    Echo Off
-    Exit /B
-)
+@echo off
+echo Hello world!
