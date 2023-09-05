6e58e14918ff178d50285e765c226681a778f84c
yellaiah a <yellaiahdevops@gmail.com>
2023-09-05 08:47:33 +0000

    test

---
+++ test.ps1
@@ -0,0 +1,14 @@
+clear-host
+ $repo = "emukidid/swiss-gc"
+ $releases = "https://api.github.com/repos/$repo/releases"
+ Write-Host Determining latest release
+ $tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+ $FileName = "swiss_"+$tag.Substring(4,5)+".7z"
+ $FilePath = "$Env:Temp\$FileName"
+ $download = "https://github.com/$repo/releases/download/$tag/$file"
+ $download
+ $FilePath
+ Write-Host Dowloading latest release
+ Try{Invoke-WebRequest $download -Out "$FilePath"} Catch {write-host "$_.Exception.Message" -fore Red;break}
+ [console]::beep(500,1000)
+ Write-Host "Download is completed and saved in this path : $FilePath" -ForegroundColor Cyan
