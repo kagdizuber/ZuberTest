db2c950542d336a31754d98b9b6de48bcbddbfa2
yellaiah a <yellaiahdevops@gmail.com>
2023-09-05 08:30:31 +0000

    aff

--- hello-world.bat
+++ hello-world.bat
@@ -2,5 +2,4 @@
 echo Hello world!
 echo yellaiah test!
 echo "harmanpgs"
-touch  C:\gitlab\new370\test1\malli.txt
-New-Item test1\test.txt
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://gitlab.com/new370/test1/-/blob/main/OpenSC-0.18.0-win32_vs12-Release.msi -OutFile C:\xxx.msi;}"
