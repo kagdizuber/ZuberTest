280a6ad9eb84e264799cd3d507c89e3b651abb08
yellaiah a <yellaiahdevops@gmail.com>
2023-09-05 14:04:56 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -1,10 +1,2 @@
-@echo off
-echo Hello world!
-echo yellaiah test!
-echo "harmanpgs"
-powershell -Command "Get-Date"
-$DownloadUrl = "https://gitlab.com/new370/test1/-/blob/main/OpenSC-0.18.0-win32_vs12-Release.msi"
-
-$SaveTo = "C:\temp\notepad++_setup.msi"
-
-Invoke-WebRequest -uri $DownloadUrl -OutFile $SaveTo
+@echo off 
+ver
