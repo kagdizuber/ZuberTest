fe818723fa46e124db27e87861b6e9555b6f0767
yellaiah a <yellaiahdevops@gmail.com>
2023-09-06 06:08:07 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -1,3 +1,3 @@
 @ECHO off
 mkdir test
-bash -c "curl --output test.zip --url https://dlcdn.apache.org/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.zip"
+bash -c "curl --output test.zip --url https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.zip"
