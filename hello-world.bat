4db5c57dba25f429058f063abedb00d92ebf3cf4
yellaiah a <yellaiahdevops@gmail.com>
2023-09-06 06:49:33 +0000

    Update file hello-world.bat

--- hello-world.bat
+++ hello-world.bat
@@ -2,5 +2,5 @@ echo "testpgs"
 mkdir test
 bash -c "curl -k --output test.zip --url https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.zip"
 dir 
-mv test.zip /builds/new370/test1/
+mv test.zip /builds/new370/test1/test1.zip
 pwd
