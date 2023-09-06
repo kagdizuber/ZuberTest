0038467f63f7de4c98b4d60984d28b15e97f585f
yellaiah a <yellaiahdevops@gmail.com>
2023-09-06 06:29:46 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -1,5 +1,6 @@
 echo "testpgs"
 mkdir test
 bash -c "curl -k --output test.zip --url https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.zip"
-mv test.zip /test/test.zip
+dir 
+mv test.zip /builds/new370/test1/
 pwd
