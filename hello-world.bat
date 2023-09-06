c010e11f0d1ea10e05e3cba354f0925b7d624e32
yellaiah a <yellaiahdevops@gmail.com>
2023-09-06 15:00:05 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -3,6 +3,9 @@ mkdir test
 bash -c "curl -k --output test.zip --url https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.zip"
 bash -c "curl -k --output new.msi --url https://gitlab.com/new370/test1/-/blob/main/OpenSC-0.18.0-win32_vs12-Release.msi"
 dir 
+
+MSIEXEC %DESTDIR\MySetup.msi, /qn
+
 @echo off
 Title HOST: Installing updates on %computername%
 echo %computername%
