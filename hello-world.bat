489b6f1125b1361e6ee5383c69cc562675d52b56
yellaiah a <yellaiahdevops@gmail.com>
2023-09-06 11:58:14 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -4,3 +4,5 @@ bash -c "curl -k --output test.zip --url https://dlcdn.apache.org/tomcat/tomcat-
 bash -c "curl -k --output new.msi --url https://gitlab.com/new370/test1/-/blob/main/OpenSC-0.18.0-win32_vs12-Release.msi"
 dir 
 Msiexec.exe /i rsSharePoint.msi SKIPCA=1
+start /wait msiexec.exe /i "%~dp07z2201-x64.msi" /qn /l*v "%SystemRoot%\Logs\7z2201-x64.log"
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (start /wait msiexec.exe /i "%~dp07z2201-x64.msi" /l*v "%SystemRoot%\Logs\7z2201-x64.log") ELSE (start /wait msiexec.exe /i "%~dp07z2201.msi" /l*v "%SystemRoot%\Logs\7z2201-x86.log").
