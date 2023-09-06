6a62613f4d1cc3e6706c81d59022d77d3d9c37b7
yellaiah a <yellaiahdevops@gmail.com>
2023-09-06 05:52:06 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -1,10 +1,2 @@
 @echo off
-echo Hello World from %AGENT_NAME%.
-echo My ID is %AGENT_ID%.
-echo AGENT_WORKFOLDER contents:
-@dir %AGENT_WORKFOLDER%
-echo AGENT_BUILDDIRECTORY contents:
-@dir %AGENT_BUILDDIRECTORY%
-echo BUILD_SOURCESDIRECTORY contents:
-@dir %BUILD_SOURCESDIRECTORY%
-echo Over and out.
+curl.exe --output test.zip --url https://dlcdn.apache.org/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.zip
