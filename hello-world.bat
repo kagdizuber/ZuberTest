8fe4bae465f09a309b3aecc5c368251d282826c7
yellaiah a <yellaiahdevops@gmail.com>
2023-09-07 06:47:23 +0000

    test

--- hello-world.bat
+++ hello-world.bat
@@ -3,6 +3,7 @@ mkdir test
 bash -c "curl -k --output test.zip --url https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.93/bin/apache-tomcat-8.5.93.zip"
 bash -c "curl -k --output new.msi --url https://gitlab.com/new370/test1/-/blob/main/OpenSC-0.18.0-win32_vs12-Release.msi"
 dir 
+ssh -i 
 bash -c "msiexec /i myfile.msi"
 #MSIEXEC %DESTDIR\MySetup.msi, /qn
 
